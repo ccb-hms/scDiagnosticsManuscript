5910dc6f315458756e9d78dd99500411474e7f18
Anthony Christidis <anthony.christidis@stat.ubc.ca>
2026-01-17 14:07:12 -0500

    Add Zenodo repository information for data download.

--- data/download_data.R
+++ data/download_data.R
@@ -1,13 +1,16 @@
 # data/download_data.R
-# Usage: source("data/download_data.R")
+# Usage: source("data/download_data.R"); download_all_data()
 
 download_all_data <- function() {
   
+  # Zenodo dataset: https://doi.org/10.5281/zenodo.18274942
+  zenodo_base <- "https://zenodo.org/records/18274942/files/"
+  
   zenodo_urls <- list(
-    covid_sce = "https://zenodo.org/.../covid_data_sce.rds",
-    covid_ref = "https://zenodo.org/.../normal_data_sce.rds",
-    merfish_sce = "https://zenodo.org/.../dss9_data.rds",
-    merfish_ref = "https://zenodo.org/.../healthy_data.rds"
+    covid_sce = paste0(zenodo_base, "covid_data_sce.rds"),
+    covid_ref = paste0(zenodo_base, "normal_data_sce.rds"),
+    merfish_sce = paste0(zenodo_base, "dss9_data.rds"),
+    merfish_ref = paste0(zenodo_base, "healthy_data.rds")
   )
   
   # Create subdirectories if needed
@@ -16,13 +19,17 @@ download_all_data <- function() {
   
   # Download COVID data
   message("Downloading COVID-19 data...")
-  download.file(zenodo_urls$covid_sce, "data/covid/covid_data_sce.rds")
-  download.file(zenodo_urls$covid_ref, "data/covid/normal_data_sce.rds")
+  download.file(zenodo_urls$covid_sce, "data/covid/covid_data_sce.rds", mode = "wb")
+  download.file(zenodo_urls$covid_ref, "data/covid/normal_data_sce.rds", mode = "wb")
   
   # Download MERFISH data
   message("Downloading MERFISH data...")
-  download.file(zenodo_urls$merfish_sce, "data/merfish/dss9_data.rds")
-  download.file(zenodo_urls$merfish_ref, "data/merfish/healthy_data.rds")
+  download.file(zenodo_urls$merfish_sce, "data/merfish/dss9_data.rds", mode = "wb")
+  download.file(zenodo_urls$merfish_ref, "data/merfish/healthy_data.rds", mode = "wb")
   
   message("✓ All data downloaded successfully!")
+  message("Data saved to data/covid/ and data/merfish/")
 }
+
+# Run this to download
+download_all_data()
